0aa6a9f58c0c20ef0fe97d3311b03537b52d929c
K-CHANHO <cksgh1565@gmail.com>
2025-06-19 18:26:49 +0900

    [test] 게시물 전체 조회 테스트

--- src/test/java/sailing/bootcamp/spring/board/repository/BoardRepositoryTest.java
+++ src/test/java/sailing/bootcamp/spring/board/repository/BoardRepositoryTest.java
@@ -7,6 +7,8 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import sailing.bootcamp.spring.board.entity.Board;
 
+import java.util.ArrayList;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -25,7 +27,7 @@ public class BoardRepositoryTest {
 
     @Test
     @DisplayName("게시글 등록 테스트")
-    public void BoardSave(){
+    public void saveBoard(){
         // given
         Board boardWithNull = Board.builder()
                 .title(null)
@@ -58,6 +60,23 @@ public class BoardRepositoryTest {
         assertThat(result.getBoardId()).isNotNull();
         assertThat(result.getTitle()).isEqualTo("게시글 제목입니당");
         assertThat(result.getContent()).isEqualTo("초록색 체크를 원합니당");
+    }
+
+    @Test
+    @DisplayName("게시물 전체 조회 테스트")
+    public void getAllBoard(){
+        // given
+        ArrayList<Board> boardList = new ArrayList<>();
+        Board testBoard1 = Board.builder().title("test1").content("테스트1").build();
+        Board testBoard2 = Board.builder().title("test2").content("테스트2").build();
+        Board testBoard3 = Board.builder().title("test3").content("테스트3").build();
+
+        // when
+        boardList.add(testBoard1); boardList.add(testBoard2); boardList.add(testBoard3);
+        boardList.stream().forEach(board->boardRepository.save(board));
+
+        // then
+        assertThat(boardRepository.findAll().size()).isEqualTo(3);
     }
 
 }
