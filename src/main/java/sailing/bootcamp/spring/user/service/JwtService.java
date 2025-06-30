e99cfbcd9bc07dff6e748634ec08377c99b2d14b
Chanho Kim <cksgh1565@gmail.com>
2025-06-30 22:33:32 +0900

    [feat] JWT 추가

---
+++ src/main/java/sailing/bootcamp/spring/user/service/JwtService.java
@@ -0,0 +1,85 @@
+package sailing.bootcamp.spring.user.service;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureAlgorithm;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import javax.crypto.SecretKey;
+import java.util.Base64;
+import java.util.Date;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JwtService {
+
+    // Header KEY 값
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    // Token 식별자
+    private static final String BEARER_PREFIX = "Bearer ";
+    // 토큰 만료시간
+    private static final long TOKEN_TIME = 60 * 60 * 1000L; // 1시간
+
+    @Value("${jwt.secret.key}")
+    private String secretKey;
+    private SecretKey key;
+    private final SignatureAlgorithm signatureAlgorithm = Jwts.SIG.RS256;
+
+
+    @PostConstruct
+    public void init(){
+        byte[] bytes = Base64.getDecoder().decode(secretKey);
+        key = Keys.hmacShaKeyFor(bytes);
+    }
+
+    // 토큰 생성
+    public String createToken(String username) {
+        Date date = new Date();
+
+        return BEARER_PREFIX +
+                Jwts.builder()
+                        .subject(username)
+                        .expiration(new Date(date.getTime() + TOKEN_TIME))
+                        .issuedAt(date)
+                        .signWith(key)
+                        .compact();
+    }
+
+    // header 토큰을 가져오기
+    public String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    // 토큰 검증
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().verifyWith(key).build().parseSignedClaims(token);
+            return true;
+        } catch (SecurityException | MalformedJwtException e) {
+            log.info("Invalid JWT signature, 유효하지 않는 JWT 서명 입니다.");
+        } catch (ExpiredJwtException e) {
+            log.info("Expired JWT token, 만료된 JWT token 입니다.");
+        } catch (UnsupportedJwtException e) {
+            log.info("Unsupported JWT token, 지원되지 않는 JWT 토큰 입니다.");
+        } catch (IllegalArgumentException e) {
+            log.info("JWT claims is empty, 잘못된 JWT 토큰 입니다.");
+        }
+        return false;
+    }
+
+    // 토큰에서 사용자 정보 가져오기
+    public Claims getUserInfoFromToken(String token) {
+        return Jwts.parser().verifyWith(key).build().parseSignedClaims(token).getPayload();
+    }
+}
